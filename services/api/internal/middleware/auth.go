32c0c8386c559e528183fbbd69c0cc8c838f6d84
Daniel Roschmann <droschmann01@gmail.com>
2026-08-03 11:08:09 +0200

    auth check

---
+++ services/api/internal/middleware/auth.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+func RequireAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+
+		userId := session.Get("userId")
+
+		if userId == nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication required"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
